681a29b39d296bedc12fb22f68445cdb0477ff38
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-02-22 19:36:45 -0600

    Complete Cache.swift

--- labs-ios-starter/Model/Cache.swift
+++ labs-ios-starter/Model/Cache.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class Cache<Key: Hashable, Value> {
+    
+    private var items: [Key: Value] = [:]
+    private let queue: DispatchQueue = .init(label: "com.LambdaSchool.LabsScaffolding.cityQueue")
+    
+    func cache(value: Value, for key: Key) {
+        queue.sync {
+            items[key] = value
+        }
+    }
+    
+    func value(for key: Key) -> Value? {
+        queue.sync {
+            items[key]
+        }
+    }
+}
