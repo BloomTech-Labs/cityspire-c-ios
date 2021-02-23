10e55ba8043e98fa76f0f0bc6011a62f725505b4
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-02-22 21:27:07 -0600

    Caching implemented, as well as cancelling a pending fetch operation. Bug exists in that detailView segue isn't being performed

--- labs-ios-starter/Cities/CitiesCollectionViewController.swift
+++ labs-ios-starter/Cities/CitiesCollectionViewController.swift
@@ -52,6 +52,46 @@ extension CitiesCollectionViewController: UICollectionViewDelegate, UICollection
         cell.cityImageView.contentMode = .scaleToFill
         cell.cityImageView.layer.cornerRadius = 15
         
+        // Check if city image is in the cache, if not, start fetch
+        
+        // Check if image in cache, if so, set image
+        if let cachedImage = cache.value(for: city) {
+            DispatchQueue.main.async {
+                cell.cityImageView.image = cachedImage
+            }
+        } else {
+            // Otherwise image not in cache, start operation to fetch image
+            getPhotoReferenceForCity(cityName: city, completion: { (photoReference) in
+                guard let photoReference = photoReference else { return }
+                let imageFetchOperation = FetchImageOperation(photoReference: photoReference)
+                
+                let cachePhotoOperation = BlockOperation {
+                    guard let imageData = imageFetchOperation.imageData,
+                        let image = UIImage(data: imageData) else { return }
+                    self.cache.cache(value: image, for: city)
+                }
+                
+                cachePhotoOperation.addDependency(imageFetchOperation)
+                
+                let setImageIfCellNotReused = BlockOperation {
+                    DispatchQueue.main.async {
+                        guard let imageData = imageFetchOperation.imageData, let image = UIImage(data: imageData) else { return }
+                        cell.cityImageView.image = image
+                    }
+                }
+                
+                setImageIfCellNotReused.addDependency(imageFetchOperation)
+                
+                self.photoFetchQueue.addOperations([
+                    imageFetchOperation,
+                    cachePhotoOperation,
+                    setImageIfCellNotReused,
+                ], waitUntilFinished: false)
+                
+                self.fetchOperations[city] = imageFetchOperation
+            })
+        }
+        
         return cell
     }
     
@@ -75,7 +115,7 @@ extension CitiesCollectionViewController: UICollectionViewDelegate, UICollection
 }
 
 extension CitiesCollectionViewController: UISearchBarDelegate {
-    // Mark: -- TextDidChange
+    // MARK: -- TextDidChange
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         let filteredCities = cities.filter { (city) -> Bool in
             return city.lowercased().starts(with: searchText.lowercased())
@@ -92,6 +132,15 @@ extension CitiesCollectionViewController {
             if let selectedCity = selectedCity, let destinationVC = segue.destination as? CityDetailViewController {
                 destinationVC.city = selectedCity
             }
+        }
+    }
+}
+
+extension CitiesCollectionViewController {
+    // MARK: -- Caching
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if let fetchOperation = fetchOperations[citySearchResults[indexPath.row]] {
+            fetchOperation.cancel()
         }
     }
 }
